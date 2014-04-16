ba5918f980ee30ab3e206d4be3eba6b097d64e16
Petr Ruzicka <petr.ruzicka@gmail.com>
2014-04-16 08:42:54 +0200

    Added configuration files for Turris (OpenWrt)

---
+++ files/turris_configured/etc/dnsmasq-script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ 
+/bin/echo `/bin/date +"%F %T"` $* >> /etc/dnsmasq.script.log
+ 
+if [ "$1" == "add" ] && ! grep -iq $2 /etc/config/dhcp; then
+  echo -e "Subject: New MAC on `uci get system.@system[0].hostname`.`uci get dhcp.@dnsmasq[0].domain`\\n\\n`/bin/date +"%F %T"` $*" | sendmail petr.ruzicka@gmail.com
+fi
